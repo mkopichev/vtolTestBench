0a58c1b94b2c79468328a9691bdb2262a206f9e4
mKopichev <mmkopychev@etu.ru>
2025-10-21 17:29:43 +0300

    max setpoint value now 255 instead of 250

--- inc/control.h
+++ inc/control.h
@@ -4,7 +4,7 @@
 #include "common.h"
 
 #define VALUE_SETPOINT 490.0
-#define VALUE_SETPOINT_MAX 250.0
+#define VALUE_SETPOINT_MAX 255.0
 #define VALUE_ERROR_INTEGRAL_MAX 50000.0
 
 void controlInit(void);
